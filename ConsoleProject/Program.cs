b06c661157a1532ae1b862fedeb93fe8103a059d
ValdimarH <hjaltasv@gmail.com>
2017-01-24 21:23:06 +0000

    make console project public

--- ConsoleProject/Program.cs
+++ ConsoleProject/Program.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace ConsoleProject
 {
-    class Program
+    public class Program
     {
-        static void Main()
+        public static void Main()
         {
         }
     }
