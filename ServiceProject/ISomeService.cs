c61aff9b721f4d59616c9e477770a707cc570ee6
Valdimar Hjaltason <Valdimar Hjaltason>
2017-01-24 21:15:30 +0000

    initial commit

---
+++ ServiceProject/ISomeService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiceProject
+{
+    [ServiceContract]
+    public interface ISomeService
+    {
+        [OperationContract]
+        void DoWork();
+    }
+}
